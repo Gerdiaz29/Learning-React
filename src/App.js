0fe55e25c92b2a0e8aa3454fb65fe5527399ed3a
Gerardo Antonio Diaz Hernandez <gerardoadh_90@hotmail.com>
2020-05-31 20:27:30 -0400

    Creating the project and css module implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TarjetaFruta from "./componentes/TarjetaFruta";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <TarjetaFruta name={"Sandia"} price={5.0}></TarjetaFruta>
+    <TarjetaFruta name={"Naranja"} price={1.5}></TarjetaFruta>
+    <TarjetaFruta name="Kiwi" price={3.3}></TarjetaFruta>
+  </div>
+);
 
 export default App;
---
+++ src/componentes/TarjetaFruta.js
@@ -0,0 +1,47 @@
+import React from "react";
+//import "./TarjetaFruta.css";
+import styles from "./TarjetaFruta.module.css";
+
+class TarjetaFruta extends React.Component {
+  state = {
+    cantidad: 0,
+  };
+
+  agregar = () => {
+    this.setState({
+      cantidad: this.state.cantidad + 1,
+    });
+  };
+
+  quitar = () => {
+    this.setState({
+      cantidad: this.state.cantidad - 1,
+    });
+  };
+
+  limpiar = () => {
+    this.setState({
+      cantidad: 0,
+    });
+  };
+
+  render() {
+    const hasItems = this.state.cantidad > 0;
+    const activeClass = hasItems ? styles["card-Active"] : "";
+    const clases = styles.card + " " + activeClass;
+    return (
+      <div className={clases}>
+        <h3>{this.props.name}</h3>
+        <div>Cantidad: {this.state.cantidad}</div>
+        <button onClick={this.agregar}>+</button>
+        <button onClick={this.quitar}>-</button>
+        <button onClick={this.limpiar}>Limpiar</button>
+        <hr />
+        <p>${this.props.price}</p>
+        <p>Total: ${this.props.price * this.state.cantidad}</p>
+      </div>
+    );
+  }
+}
+
+export default TarjetaFruta;
