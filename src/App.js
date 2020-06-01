8a5b5484b9804e7e6afda49f23d39b9fa1cf4180
Gerardo Antonio Diaz Hernandez <gerardoadh_90@hotmail.com>
2020-06-01 16:25:44 -0400

    using modules.css and component modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import TarjetaFruta from "./componentes/TarjetaFruta";
+import TarjetaFruta from "./components/TarjetaFruta";
+const frutas = [
+  { id: 1, name: 1, price: "5" },
+  { id: 2, name: "Naranja", price: 1.5 },
+  { id: 3, name: "Kiwi", price: 3.3 },
+];
 
 const App = () => (
   <div>
-    <TarjetaFruta name={"Sandia"} price={5.0}></TarjetaFruta>
-    <TarjetaFruta name={"Naranja"} price={1.5}></TarjetaFruta>
-    <TarjetaFruta name="Kiwi" price={3.3}></TarjetaFruta>
+    {frutas.map((fruta) => (
+      <TarjetaFruta key={fruta.id} fruta={fruta}></TarjetaFruta>
+    ))}
   </div>
 );
 
--- src/components/TarjetaFruta/TarjetaFruta.js
+++ src/components/TarjetaFruta/TarjetaFruta.js
@@ -1,10 +1,16 @@
 import React from "react";
-//import "./TarjetaFruta.css";
 import styles from "./TarjetaFruta.module.css";
+import PropTypes from "prop-types";
 
 class TarjetaFruta extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    price: PropTypes.number,
+  };
+
   state = {
     cantidad: 0,
+    fruta: this.props.fruta,
   };
 
   agregar = () => {
@@ -26,19 +32,23 @@ class TarjetaFruta extends React.Component {
   };
 
   render() {
-    const hasItems = this.state.cantidad > 0;
+    const { name, price } = this.state.fruta;
+    const { cantidad } = this.state;
+
+    const hasItems = cantidad > 0;
     const activeClass = hasItems ? styles["card-Active"] : "";
     const clases = styles.card + " " + activeClass;
+
     return (
       <div className={clases}>
-        <h3>{this.props.name}</h3>
-        <div>Cantidad: {this.state.cantidad}</div>
+        <h3>{name}</h3>
+        <div>Cantidad: {cantidad}</div>
         <button onClick={this.agregar}>+</button>
         <button onClick={this.quitar}>-</button>
         <button onClick={this.limpiar}>Limpiar</button>
         <hr />
-        <p>${this.props.price}</p>
-        <p>Total: ${this.props.price * this.state.cantidad}</p>
+        <p>${price}</p>
+        <p>Total: ${price * cantidad}</p>
       </div>
     );
   }
